eb7fed4db9711fd4d15a971e7e1a10007bb92975
unknown <sok>
2022-08-17 19:44:07 +0700

    #18 create new request form

---
+++ front/src/components/Newrequest/Newrequest.vue
@@ -0,0 +1,223 @@
+<template>
+  <div class="about">
+    <form @submit.prevent="reQuest" class="leave_request">
+      <div class="heading_form">
+        <h1>Request Leave Form</h1>
+      </div>
+      <div class="support_heading"></div>
+      <div class="content">
+        <div class="title">
+          <h5>Leave type :</h5>
+        </div>
+        <div class="box">
+          <select v-model="leave_type" id="leave_type">
+            <option>choose..</option>
+            <option value="busy">Busy</option>
+            <option value="sick">Sick</option>
+          </select>
+        </div>
+      </div>
+      <div class="content">
+        <div class="title">
+          <h5>Date start</h5>
+        </div>
+        <div class="box">
+          <input type="date" v-model="start_date"/>
+          <select v-model="start_date_status">
+            <option value="am">AM</option>
+            <option value="pm">PM</option>
+          </select>
+        </div>
+      </div>
+      <div class="content">
+        <div class="title">
+          <h5>Date end</h5>
+        </div>
+        <div class="box">
+          <input type="date" v-model="end_date">
+          <select v-model="end_date_status">
+            <option value="am">AM</option>
+            <option value="pm">PM</option>
+          </select>
+        </div>
+      </div>
+      <div class="duration">
+        <h5>Duration: 3 day</h5>
+      </div>
+      <div class="content2">
+        <div class="title">
+          <h5>Cause (Reason):</h5>
+        </div>
+        <div class="box">
+          <textarea  id="reason" v-model="reason" cols="40" rows="5" placeholder=" you must have reason..."></textarea>
+        </div>
+      </div>
+      <div class="footer">
+        <button @click="cancleR" id="cancle">Cancle</button>
+        <button @click="submitR" id="submit">Submit</button>
+      </div>
+  </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Newrequest_Form",
+  emits:['new-request'],
+  data(){
+    return {
+      leave_type:[],
+      start_date:'',
+      start_date_status:'',
+      end_date:'',
+      end_date_status:'',
+      reason:'',
+    }
+  },
+  methods:{
+    cancleR(){
+      this.clearForm();
+    },
+    submitR(){
+      if (
+        this.leave_type != [] &&
+        this.start_date != '' &&
+        this.start_date_status != ''&&
+        this.end_date != '' && 
+        this.end_date_status != '' && 
+        this.reason != ''
+      ){
+        this.$emit('new-request',{
+          leave_type:this.leave_type,
+          startDate:this.start_date,
+          startDateStatus:this.start_date_status,
+          endDate:this.end_date,
+          endDateStatus:this.end_date_status,
+          reason:this.reason
+        });
+        this.clearForm();
+      }else {
+        alert('You missing some field please check again!');
+      }
+    },
+    clearForm(){
+      this.leave_type = [];
+      this.start_date='';
+      this.start_date_status = '';
+      this.end_date = '';
+      this.end_date_status = '';
+      this.reason = '';
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .about {
+    width: 100%;
+    display: flex;
+  }
+  .leave_request {
+    width: 35%;
+    display: flex;
+    flex-wrap: wrap;
+    margin:auto;
+    background-color: rgb(255, 255, 255);
+    font-family:sans-serif;
+    border-radius: 20px;
+    margin-top:100px;
+    height: 75vh;
+  }
+  .heading_form {
+    width: 100%;
+    border-radius: 20px;
+    background-color: rgb(13, 163, 232);
+    text-align: center;
+    align-items: center;
+  }
+  .heading_form h1 {
+    font-size: 25px;
+    color: white;
+  }
+  .heading_form h1 {
+    margin-top: 25px;
+  }
+  .support_heading {
+    width: 100%;
+    background-color: rgb(13, 163, 232);
+    padding: 1px;
+    margin-top: -17px;
+  }
+  .content, .content2 {
+    width: 90%;
+    display: flex;
+    flex-wrap: wrap;
+    margin:5px auto;
+  }
+  .title {
+    width: 30%;
+    text-align: start;
+    margin: auto;
+    
+  }
+  .duration {
+    width:88%;
+    text-align: start;
+    margin: auto;
+  }
+  .box {
+    width: 67%;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+  #leave_type {
+    width:95% ;
+  }
+  .box input, .box select {
+    width: 45%;
+    margin-left: 10px;
+  }
+  .content2 .box {
+    width: 90%;
+    margin: 5px auto;
+  }
+  .content2 .title {
+    width: 97%;
+    margin: auto;
+  }
+  .content2 .box {
+    width: 98%;
+  }
+  .content2 .title {
+    text-align: start;
+  }
+  .content2 .box textarea {
+    width: 100%;
+    resize: none;
+    background-color: rgb(163, 163, 163);
+    color: white;
+  }
+ ::placeholder {
+    color: white;
+  }
+  .footer {
+    width: 93%;
+    margin: 10px auto;
+    text-align: end;
+  }
+  .footer button {
+    padding: 8px;
+    margin: 2px;
+    border: none;
+    border-radius: 10px;
+    color:white;
+    cursor: pointer;
+  }
+  #cancle {
+    background-color: orange;
+  }
+  #submit {
+    background-color: rgb(13, 163, 232) ;
+  }
+</style>
--- front/src/views/AboutView.vue
+++ front/src/views/AboutView.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <Newrequest/>
 </template>
+<script>
+import Newrequest from '../components/Newrequest/Newrequest.vue';
+export default {
+  name:"About-view",
+  components:{
+    Newrequest,
+  }
+}
+</script>
 <style scoped>
 
-.about {
-  box-sizing: border-box;
-  margin-top: 5%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: #cccc;
-}
 </style>
