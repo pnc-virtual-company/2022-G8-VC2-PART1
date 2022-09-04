e6e808516990f97e32f24d3eeea59d6d3773091c
Hinsoklim <soklim.hin@student.passerellesnumeriques.org>
2022-09-04 15:09:54 +0700

    Update Mail

--- back/resources/views/mailer/mailer.blade.php
+++ back/resources/views/mailer/mailer.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
+
 <body>
-    <h1>Hello User</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium assumenda provident, dicta dolore cum voluptatibus iure omnis fuga! Vero autem ipsum nemo rerum iusto culpa et dignissimos sed assumenda omnis.</p>
-    <but<body>
     <div class="main" style="padding: 5px;">
         <strong>Dear Teacher,</strong>
         <p>I would like to ask permision :</p>
@@ -21,6 +13,4 @@
             <a href="http://localhost:8080/login" style="background: red; text-decoration: none; color: white; padding: 5px; border-radius: 10px; margin: 5px;">Reject</a>
         </div>
     </div>
-</body>ton>Hello</button>
 </body>
-</html>
